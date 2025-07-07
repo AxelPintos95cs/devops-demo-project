1cf137eef58f3cbec6125ad6128073f0595d3f08
Axel <axel95.cs@hotmail.com>
2025-07-07 15:37:51 -0300

    V0.2 - AWS Connect - TF Destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,23 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 resource "aws_instance" "flask_server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
-  instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  instance_type          = "t2.micro"
+  associate_public_ip_address = true
 
   tags = {
     Name = "FlaskAppServer"
   }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras install docker -y
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+              docker run -d -p 80:5000 axel/flask-app
+              EOF
 }
+
